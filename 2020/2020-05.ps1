3fd318d4ca570f79bed8c9567849d33cc663dbc6
Allen <Jonathan.Allen@quilter.com>
2020-12-04 18:01:12 +0000

    template ready

---
+++ 2020/2020-05.ps1
@@ -0,0 +1,20 @@
+# Advent of code
+# Jonathan Allen
+
+
+$AOC = "C:\Users\alljon\OneDrive - Quilter\Scripts\PowerShell\AdventofCode"
+sl $AOC
+$One = get-content .\SourceData\20201205.txt
+
+
+#region ### part 1 ####
+# problem statement:
+
+
+#endregion #######
+
+
+
+#region ### part 2 ####
+
+#endregion #######
